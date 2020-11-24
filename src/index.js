e69876e8e960af111cb4bcd492fbc97ee6b7e88e
Alan <alan@remote.com>
2020-11-24 11:23:14 -0600

    price inquiry now works

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Discord from "discord.js"
 import MongoClient from 'mongodb'
+import load from './filereader.js'
+
 
 const USERNAME=`food`
 const PASSWORD=`bot`
@@ -9,6 +11,14 @@ const COLL_NAME = `food`
 const MONGO_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.u6lhh.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
 const CLIENT = new Discord.Client()
 const PREFIX = `$`
+
+var concat = (arr) => 
+{
+    arr.shift()
+    let str = ""
+    arr.forEach(element => str+=element+=' ');
+    return str.trim()
+}
 
 
 CLIENT.once(`ready`, () => console.log("food bot is online"))
@@ -22,6 +32,8 @@ MongoClient.connect(MONGO_URL, OPTIONS).then(client =>
 
     CLIENT.on('message', message =>
     {
+        const BOT = message.channel
+
         if (!message.content.startsWith(PREFIX))
         {
             console.log(`Usage: ${PREFIX}<command>`)
@@ -37,35 +49,49 @@ MongoClient.connect(MONGO_URL, OPTIONS).then(client =>
             switch (command)
             {
                 case 'price':
-                    let resturaunt = args[1]
+                    let resturaunt = concat(args)
                     let criteria = 
                     {
-                        Name: resturaunt
+                        Name: 
+                        {
+                            $regex: `${resturaunt}`, 
+                            $options: "si"
+                        }
                     }
                     COLLECTION.findOne(criteria, (err, res) =>
                     {
                         console.log('inside find')
                         if (err) console.log(err)
                         console.log(res)
-                        message.channel.send(`${res.Name}: ${res.Price}`)
+                        try 
+                        {
+                            BOT.send(`${res.Name}: ${res.Price}`)
+                        } 
+                        catch (error) 
+                        {
+                            BOT.send(`Error: Cannot find ${args}`)
+                        }
+                        
                     })
                     console.log('before break')
                     break
                 case 'rate':
                     console.log('rate')
                     break
+                case '':
                 case ' ':
                     console.error("invalid command")
                     break
                 default:
-                    message.channel.send(`https://hbomax-images.warnermediacdn.com/images/GXoZVkwh6YrLCwgEAAACJ/tileburnedin?size=1280x720&format=jpeg&partner=hbomaxcom&productCode=hbomax&host=artist.api.cdn.hbo.com&w=1200`)
+                    BOT.send(`https://hbomax-images.warnermediacdn.com/images/GXoZVkwh6YrLCwgEAAACJ/tileburnedin?size=1280x720&format=jpeg&partner=hbomaxcom&productCode=hbomax&host=artist.api.cdn.hbo.com&w=1200`)
                     break
             }
                     
         }
     })
 })
+.catch(err => console.log(err))
 
 
-const TOKEN=`ENTER TOKEN HERE`
+const TOKEN= `` //enter token here
 CLIENT.login(TOKEN)
